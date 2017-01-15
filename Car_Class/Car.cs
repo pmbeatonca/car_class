d044693dd8ecde25f8c9638ff325593da865e489
pablo martinez <pablo martinez>
2017-01-15 17:43:41 -0500

    Initial commit after completing the project

---
+++ Car_Class/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class
+{
+    class Car
+    {
+        //public Properties
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public int Speed { get; set; }
+
+        //constructor method with two arguments or parameters
+
+        public Car(int year,string make)
+        {
+            Year = year;
+            Make = make;
+            Speed = 0;
+        }
+        public void Accelerate()
+        {
+            Speed += 5; //or Speed=Speed+5
+        }
+        public void Break()
+        {
+            Speed -= 5;
+        }
+
+    }
+               
+}
---
+++ Car_Class/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Car_Class
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Car car;
+        public MainWindow()
+        {
+            InitializeComponent();
+     
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string make = txtMake.Text;
+            int year = Convert.ToInt32(txtYear.Text);
+            car = new Car(year, make);
+            lblMake.Content = car.Make;
+            lblSpeed.Content = car.Speed;
+            lblYear.Content = car.Year;
+        }
+
+        private void btnAcc_Click(object sender, RoutedEventArgs e)
+        {
+            car.Accelerate();
+            lblSpeed.Content = car.Speed;          
+
+        }
+
+        private void Brake_Click(object sender, RoutedEventArgs e)
+        {
+            car.Break();
+            lblSpeed.Content = car.Speed;
+        }
+    }
+}
